37cc2b6204b6fccb92619d7947651f3603bdbef7
Develops-Master <developsmaster@gmail.com>
2026-04-10 02:50:20 -0500

    Base1

---
+++ GestionesCIST.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestionesCIST.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
